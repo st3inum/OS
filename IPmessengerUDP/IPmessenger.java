b025f7422a22bac064d1232e9888d40f59b77633
st3inum <st3inum@gmail.com>
2019-08-27 15:57:52 +0600

    Add files via upload

--- IPmessengerUDP/IPmessenger.java
+++ IPmessengerUDP/IPmessenger.java
@@ -6,15 +6,19 @@ import java.util.Scanner;
 
 class IPmessenger {
 	public static void main(String args[]) throws UnknownHostException, IOException {
-		System.out.println("1)send");
-		System.out.println("2)receive");
-		System.out.println("3)exit");
-		Scanner sc = new Scanner(System.in);
-		int id = sc.nextInt();
-		if (id == 1) {
-			Client client = new Client("127.0.0.1", 5000);
-		} else if (id == 2) {
-			Server server = new Server(5000);
-		} else return;
+		while(true)
+                {
+                    System.out.println("1)send");
+                    System.out.println("2)receive");
+                    System.out.println("3)exit");
+                    Scanner sc = new Scanner(System.in);
+                    int id = sc.nextInt();
+                    if (id == 1) {
+                            
+                            Client client = new Client("10.100.51.156", 5000);
+                    } else if (id == 2) {
+                            Server server = new Server(5000);
+                    } else return;
+                }
 	}
 }
